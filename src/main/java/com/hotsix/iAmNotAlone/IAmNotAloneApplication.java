693facdc4d6fca73e04bcd906af7fed9ba31084c
hikihun <skh140@naver.com>
2023-05-31 23:01:30 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/hotsix/iAmNotAlone/IAmNotAloneApplication.java
@@ -0,0 +1,13 @@
+package com.hotsix.iAmNotAlone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAmNotAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAmNotAloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotsix/iAmNotAlone/IAmNotAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotsix.iAmNotAlone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAmNotAloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
